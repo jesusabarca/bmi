37026662e8bb63ab2b9e9211358cc15e0b4c85a1
J Jesús Abarca Vargas <jesus@abarca.xyz>
2019-01-06 14:30:51 -0600

    Adds an ability file.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
   include Secured
+
+  def current_user
+    @current_user ||= User.where(email: email_from_session).first_or_create! do |user|
+      user.name = name_from_session
+    end
+  end
+
+  private
+
+  def email_from_session
+    session[:userinfo].to_h.dig 'info', 'email'
+  end
+
+  def name_from_session
+    session[:userinfo].to_h.dig 'info', 'name'
+  end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :user do
     email { 'email@fake.com' }
+    name { 'Jesús Abarca' }
   end
 end
