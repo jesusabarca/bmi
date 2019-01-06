9683997c0f0ef151a2ee44b2193576fabcb84a4d
J Jesús Abarca Vargas <jesus@abarca.xyz>
2019-01-06 13:21:56 -0600

    Fixes creds retrieval in testing.

--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -4,7 +4,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider(
     :auth0,
     Rails.configuration.auth0['client_id'],
-    Rails.application.credentials.auth0[:client_secret],
+    Rails.application.credentials.auth0.try(:fetch, :client_secret),
     Rails.configuration.auth0['namespace'],
     callback_path: Rails.configuration.auth0['callback_path'],
     authorize_params: {
